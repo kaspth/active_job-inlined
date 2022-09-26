94fa1db05aa5f333a23931fcee5af9823ac367bc
Kasper Timm Hansen <kaspth@gmail.com>
2022-09-26 13:48:47 +0200

    Start fleshing out our tests

--- test/active_job/test_inlined.rb
+++ test/active_job/test_inlined.rb
@@ -2,18 +2,30 @@
 
 require "test_helper"
 
-class ActiveJob::TestInlined < Minitest::Test
-  def setup
-    super
-    Post.published = false
-  end
+class ActiveJob::TestInlined < ActiveSupport::TestCase
+  include ActiveJob::TestHelper
 
-  def test_that_it_has_a_version_number
+  setup { Post.published = false }
+
+  test "version number" do
     refute_nil ::ActiveJob::Inlined::VERSION
   end
 
-  def test_running_inlined
-    Post::PublishJob.inlined.perform_later
+  test "nothing was broken" do
+    assert_enqueued_jobs 1, only: Post::PublishJob do
+      Post::PublishJob.perform_later
+    end
+
+    perform_enqueued_jobs
+    assert Post.published
+  end
+
+  test "running inlined outside of job enqueues" do
+    assert_enqueued_jobs 1, only: Post::PublishJob do
+      Post::PublishJob.inlined.perform_later
+    end
+
+    perform_enqueued_jobs
     assert Post.published
   end
 end
