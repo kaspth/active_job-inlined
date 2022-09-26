5081ffbe097809714efaca99393ce68d19e832f9
Kasper Timm Hansen <kaspth@gmail.com>
2022-09-26 13:15:42 +0200

    Setup the usual project details

--- test/active_job/test_inlined.rb
+++ test/active_job/test_inlined.rb
@@ -6,8 +6,4 @@ class ActiveJob::TestInlined < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::ActiveJob::Inlined::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
